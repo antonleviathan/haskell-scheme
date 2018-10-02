b151ca5f3bc1817b925a545acb9889de284edc75
antonleviathan <anton.livaja@gmail.com>
2018-10-02 09:44:53 +0200

    Write parsers for String, Number and Atom

--- Main.hs
+++ Main.hs
@@ -1,5 +1,6 @@
 import Text.ParserCombinators.Parsec hiding (spaces)
 import System.Environment
+import Control.Monad
 
 data LispVal = Atom String
              | List [LispVal] 
@@ -8,17 +9,42 @@ data LispVal = Atom String
              | String String 
              | Bool Bool
 
+spaces :: Parser ()
+spaces = skipMany1 space
+
 symbol :: Parser Char
 symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
 
+parseString :: Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+parseAtom :: Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol)  
+  let atom = first:rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _    -> Atom atom
+
+parseNumber :: Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr :: Parser LispVal
+parseExpr = parseAtom
+    <|> parseString
+    <|> parseNumber
+
 readExpr :: String -> String
 readExpr input = 
-  case parse (spaces >> symbol) "lisp" input of
+  case parse parseExpr "lisp" input of
     Left err  -> "No match: " ++ show err
     Right val -> "Found value"                    
-
-spaces :: Parser ()
-spaces = skipMany1 space
 
 main :: IO ()
 main = do
